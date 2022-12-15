42c6e0e13e20aadf51fcc73d94c3f32751a132da
GiraudoMauricio <giraudo64@gmail.com>
2022-12-15 19:27:03 -0300

    comentarios y validaciones

--- modificar_datos_personales.py
+++ modificar_datos_personales.py
@@ -3,73 +3,94 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-"""Seleccionamos el driver de chrome para realizar la prueba"""
+# Seleccionamos el driver de chrome para realizar la prueba
 driver = webdriver.Chrome()
 
-"""Maximizamos la pantalla para realizar la prueba"""
+# Maximizamos la pantalla para realizar la prueba
 driver.maximize_window()
 
-"""Ingresar a la web"""
+# 1-Acceder al link
 driver.get("https://www.arredo.com.ar")
 time.sleep(2)
 
-"""Clickear en la opcion para iniciar sesion"""
+# 2-Clickear en la opción para iniciar sesión ubicado en la parte superior derecha de la pagina web.
 driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[1]/div/div/div/div[2]/div/div[1]/div/div[3]/div/div/div[2]/div/div/button').click()
 time.sleep(8)
 
-"""Clickear en la opcion 'Recibir código de acceso por e-mail'"""
+# 3-Clickear en la opción "Recibir código de acceso por e-mail".
 driver.find_element(By.XPATH, '/html/body/div[7]/div/div/div/div/div/div[2]/div/div/div/div[1]/ul/li[1]/div/button/div').click()
 time.sleep(2)
 
-"""Igresar el correo electronico en el input"""
+# 4-Ingresar el correo electrónico en el campo "email" 
 email = driver.find_element(By.NAME, 'email')
 email.send_keys('fruxafrauquoppe-8760@yopmail.com')
+
+# 5-Clickear en la opcion "ENVIAR" ubicado abajo del campo "email" 
 email.send_keys(Keys.ENTER)
 time.sleep(15)
 
-# """Clickear en la opcion 'ENVIAR' """
-# driver.find_element(By.XPATH, '/html/body/div[7]/div/div/div/div/div/div[2]/div/div/div/div/form/div[2]/div[2]/button/div/span').click()
-# time.sleep(18)
+# 6-Ingresar el código enviado al correo electrónico ingresado en el campo "token"
+#                   ------------ PASO MANUAL ------------
 
-"""Clickear en la opcion 'CONFIRMAR' """
+
+# 7-Clickear en el botón "CONFIRMAR" para enviar el código de validación. 
 driver.find_element(By.XPATH, '/html/body/div[7]/div/div/div/div/div/div[2]/div/div/div/div/form/div[2]/div[2]/button/div/span').click()
 time.sleep(10)
 
-"""Clickear en la opcion para cerrar el POP UP"""
+
+# 8-Validar que el token ingresado coincida con el token que fue enviado por mail 
+verificacion_login = driver.find_element(
+    By.XPATH, '/html/body/div[2]/div/div[1]/div/div[1]/div/div/div/div[2]/div/div[1]/div/div[3]/div/div/div[2]/div/div/button/div/span/div/div')
+assert verificacion_login.text == "Holaaa"
+
+
+# 9-Clickear en la opcion para cerrar el POP UP
 driver.find_element(By.XPATH, '//*[@id="btnNoIdWpnPush"]').click()
 time.sleep(5)
 
-"""Clickear en la opcion para acceder al perfil"""
+# 10-Validar que el usuario ingreso al home a través de su cuenta
+
+# 11-Clickear en la opcion para acceder al perfil
 driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[1]/div/div/div/div[2]/div/div[1]/div/div[3]/div/div/div[2]/div/div/button').click()
 time.sleep(5)
 
-"""Clicker en la opcion 'Mi cuenta'"""
+#  12-Clicker en la opcion 'Mi cuenta'
 driver.find_element(By.XPATH, '/html/body/div[7]/div/div/div/div/div/div[2]/div/div/div/div/div[1]/a/button/span').click()
 time.sleep(8)
 
-"""Clickear en la opcion para editar los datos personales"""
+
+#  13-Validar que se visualiza el botón para editar la información personal
+
+
+#  14-Clickear en la opcion para editar los datos personales
 driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[2]/div/div/div/div/section/main/main/div[1]/div/div/article/footer/button').click()
 time.sleep(5)
 
-"""Clickear en la opcion para ingresar el nombre de usuario"""
+#  15-Clickear en el campo "Nombre" e ingresar el nombre del usuario
 nombre = driver.find_element(By.NAME, 'firstName')
 nombre.send_keys('Agente')
 time.sleep(5)
 
-"""Clickear en la opcion para ingresar Apellido"""
+#  16-Clickear en el campo "Apellido" e ingresar el apellido del usuario
 apellido = driver.find_element(By.NAME, 'lastName')
 apellido.send_keys('007')
 time.sleep(5)
 
-"""Clickear en la opcion para ingresar el documento"""
+#  17-Clickear en el campo "DNI" e ingresar un numero de documento
 documento = driver.find_element(By.NAME, 'document')
 documento.send_keys('85456321')
 time.sleep(5)
 
-"""Clickear en la opcion para guardar la informacion personal"""
+
+#  18-Validar que el número de documento tenga solo 8 caracteres y este solo conformado por números
+
+
+#  19-Clickear en el botón 'Guardar cambios' """
 driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[2]/div/div/div/div/section/main/div/div/article/main/form/button/div').click()
 time.sleep(5)
 
-"""Clickear en el logo de la pagina ubicado en la parte superior de la pantalla para volver al home de la pagina web"""
+#  20-Clickear sobre el logo de arredo ubicado en la parte superior de la pagina.
 driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[1]/div/div/div/div[2]/div/div[1]/div/div[2]/a').click()
 time.sleep(15)
+
+#  21- Validar que el usuario volvió al home
--- registro_usuario.py
+++ registro_usuario.py
@@ -9,33 +9,50 @@ driver = webdriver.Chrome()
 """Maximizamos la pantalla para realizar la prueba"""
 driver.maximize_window()
 
-"""Ingresar a la web"""
+# 1-Acceder al link https://www.arredo.com.ar/login
 driver.get("https://www.arredo.com.ar/login")
 time.sleep(2)
 
-"""Clickear en la opcion 'ENTRAR CON E-MAIL Y CONTRASEÑA'"""
+# 2-Clickear en la opción "ENTRAR CON E-MAIL Y CONTRASEÑA"
 driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/ul/li[2]/div/button/div').click()
 time.sleep(2)
 
-"""Clickear en la opcion '¿No tiene una cuenta? Regístrese'"""
+# 3-Validar que se visualice la opción para crear una cuenta
+
+# 4-Clickear en la opción "¿No tiene una cuenta? Regístrese"
 driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[2]/div/div/div/div/div/div/form/div[5]/a').click()
 time.sleep(2)
 
-"""Clickear en la opcion para ingresar el mail"""
+# 5-Clickear en la opción para ingresar el mail
 usuario = driver.find_element(By.NAME, 'email')
 time.sleep(2)
 
-"""Ingresar el correo electronico"""
+# 6-Ingresar el correo electronico en el campo "email"
 usuario.send_keys("fruxafrauquoppe-8760@yopmail.com")
+
+# 7-Clickear en la botón "ENVIAR" debajo del formulario del correo electrónico
 usuario.send_keys(Keys.ENTER)
 time.sleep(7)
 
-"""Ingresar la contraseña"""
+# 8-Ingresar el código enviado al mail anteriormente ingresado
+# PASO MANUAL
+
+# 9-Ingresar la contraseña en el campo "Ingrese su contraseña"
 password = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[2]/div/div/div/div/div/div/form/div[2]/div/label/div/input')
 password.send_keys('123456789Ma')
 time.sleep(2)
 
-"""Ingresar nuevamente la contraseña"""
+# 10-Validar que la contraseña cumple con los requisitos mínimos
+
+# 11-Ingresar nuevamente la contraseña en el campo "Confirmar contraseña"
 confirmar_password = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[2]/div/div/div/div/div/div/form/div[3]/label/div/input')
 confirmar_password.send_keys('123456789Ma')
 time.sleep(30)
+
+# 12- Validar que ambas contraseñas ingresadas coincidan
+
+# 13- Clickear en la opción "CREAR" ubicado abajo del campo "Confirmar contraseña"
+
+# 14- Validar que el token ingresado sea el correcto
+
+# 15- Validar que el usuario es redirigido a la home de la pagina web
